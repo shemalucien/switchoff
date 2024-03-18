083099c360eee2115bc1d5c91e677ef586061e61
shemalucien <shemalucien5@gmail.com>
2024-03-18 12:25:53 +0200

    fixex navbar errors

--- apps/web/src/app/navbar.tsx
+++ apps/web/src/app/navbar.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import Link from "next/link";
 import { FaBars, FaTimes } from "react-icons/fa";
@@ -31,7 +30,9 @@ const Navbar = () => {
     return (
         <div className="flex justify-between items-center w-full h-20 px-4 text-black bg-white fixed nav shadow-md z-50">
             <div>
-                <Image src={logo} alt="Logo" width={50} height={40} onClick={() => { setNav(false); }} />
+                <button onClick={() => { setNav(false); }} aria-label="Logo">
+                    <Image src={logo} alt="Logo" width={50} height={40} />
+                </button>
             </div>
 
             <ul className="hidden md:flex">
@@ -48,31 +49,20 @@ const Navbar = () => {
                 </li>
             </ul>
 
-            <div
+            <button
                 onClick={() => setNav(!nav)}
                 className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
-                tabIndex={0} // Make the div focusable
-                onKeyDown={(e) => {
-                    if (e.key === 'Enter') {
-                        setNav(!nav);
-                    }
-                }} // Add keyboard event listener
+                aria-label="Toggle navigation"
             >
                 {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
-            </div>
+            </button>
 
             {nav && (
-                <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b  from-cyan-500 to-blue-500 text-white">
+                <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-cyan-500 to-blue-500 text-white">
                     {links.map(({ id, link }) => (
                         <li
                             key={id}
                             className="px-4 cursor-pointer capitalize py-6 text-4xl"
-                            tabIndex={0} // Make the li focusable
-                            onKeyDown={(e) => {
-                                if (e.key === 'Enter') {
-                                    setNav(!nav);
-                                }
-                            }} // Add keyboard event listener
                         >
                             <Link href={link}>
                                 {link}
