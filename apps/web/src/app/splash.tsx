25791ac04ff663c8034a8fa78104cbb928f8fd13
shemalucien <shemalucien5@gmail.com>
2024-07-10 20:42:08 +0200

    changed splash screen to fetch theme from local storage

--- apps/web/src/app/splash.tsx
+++ apps/web/src/app/splash.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, FormEvent} from 'react';
+import React, { useState, FormEvent, useEffect} from 'react';
 import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
 import { FaFacebook, FaTwitter, FaInstagram, FaYoutube } from 'react-icons/fa';
@@ -7,40 +7,40 @@ import underage from '../../public/images/18.png';
 import dontdrink from '../../public/images/dontdrink.png';
 import pregnancy from '../../public/images/pregnant.png';
 import logo from '../../public/images/logo.png';
+
+
 // Define the prop types for SplashPage
 interface SplashPageProps {
     onAccessGranted: () => void; // This function will be called when access is granted
 }
 
-// inferface error
+// Interface error
 interface Error {
     message: string;
     imageUrl: StaticImageData;
 }
 
 const SplashPage: React.FC<SplashPageProps> = ({ onAccessGranted }) => {
-    // const [dob, setDob] = useState<string>('');
-    const [day, setDay] = useState("");
-    const [month, setMonth] = useState("");
-    const [year, setYear] = useState("");
+    const [day, setDay] = useState<string>('');
+    const [month, setMonth] = useState<string>('');
+    const [year, setYear] = useState<string>('');
     const [error, setError] = useState<Error | null>(null);
+    const [darkMode, setDarkMode] = useState<boolean>(false);
 
-    // // Check local storage for access status on component mount
-    // useEffect(() => {
-    //     const accessGranted = localStorage.getItem('accessGranted');
-    //     if (accessGranted === 'true') {
-    //         onAccessGranted();
-    //     }
-    //     // Explicitly return undefined to avoid potential ESLint warnings
-    //     return undefined;
-
-    // }, [onAccessGranted]);
-
+    useEffect(() => {
+        const savedTheme = localStorage.getItem('theme');
+        if (savedTheme === 'dark') {
+            setDarkMode(true);
+            document.documentElement.classList.add('dark');
+        } else {
+            setDarkMode(false);
+            document.documentElement.classList.remove('dark');
+        }
+    }, []);
 
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
         const today = new Date();
-        // const birthDate = new Date(dob);
         const birthDate = new Date(`${year}-${month}-${day}`);
         let age = today.getFullYear() - birthDate.getFullYear();
         const m = today.getMonth() - birthDate.getMonth();
@@ -53,12 +53,7 @@ const SplashPage: React.FC<SplashPageProps> = ({ onAccessGranted }) => {
                 imageUrl: underage,
             });
         } else {
-            // // Store access status in local storage
-            // localStorage.setItem('accessGranted', 'true');
-
-            // Store access status in local storage with expiry time
             setWithExpiry('accessGranted', 'true', 3600000); // 3600000 milliseconds = 1 hour
-            // Call the onAccessGranted function to indicate access is granted
             onAccessGranted();
         }
     };
@@ -72,38 +67,38 @@ const SplashPage: React.FC<SplashPageProps> = ({ onAccessGranted }) => {
         };
         localStorage.setItem(key, JSON.stringify(item));
     }
+
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
-            <div className=" bg-white dark:text-black flex flex-col items-center justify-center border-2 w-full sm:w-1/2 mx-autop-8 rounded-lg shadow-lg">
+        <div className={`flex items-center justify-center min-h-screen ${darkMode ? 'bg-gray-900' : 'bg-gradient-to-r from-cyan-500 to-blue-500'}`}>
+            <div className={`bg-white ${darkMode ? 'text-black' : 'text-gray-800'} flex flex-col items-center justify-center border-2 w-full sm:w-1/2 mx-auto p-8 rounded-lg shadow-lg`}>
                 <div className="flex items-center justify-center mb-8">
-                    {/* Social Media Icons */}
                     <Link href="#">
-                        <FaFacebook className="text-gray-800 " />
+                        <FaFacebook className={darkMode ? 'text-white' : 'text-gray-800'} />
                     </Link>
                     <Link href="#">
-                        <FaTwitter className="text-gray-800 " />
+                        <FaTwitter className={darkMode ? 'text-white' : 'text-gray-800'} />
                     </Link>
                     <Link href="#">
-                        <FaInstagram className="text-gray-800 " />
+                        <FaInstagram className={darkMode ? 'text-white' : 'text-gray-800'} />
                     </Link>
                     <Link href="#">
-                        <FaYoutube className="text-gray-800 " />
+                        <FaYoutube className={darkMode ? 'text-white' : 'text-gray-800'} />
                     </Link>
                 </div>
-                <h1 className="text-3xl font-bold mb-8 text-center dark:text-black ">Welcome</h1>
+                <h1 className="text-3xl font-bold mb-8 text-center">Welcome</h1>
                 <div className="text-center flex flex-col md:flex-row gap-8">
                     <div className="flex flex-col items-center justify-center">
                         <Image src={logo} alt="Logo" width={200} height={200} />
                     </div>
-                    <div className="flex flex-col items-center justify-center dark:text-black ">
-                        <p className="text-lg font-bold mb-4 dark:text-black ">Please enter your date of birth</p>
+                    <div className="flex flex-col items-center justify-center">
+                        <p className="text-lg font-bold mb-4">Please enter your date of birth</p>
                         <form onSubmit={handleSubmit} className="space-y-4 flex flex-col">
                             <div className="flex space-x-2">
                                 <input
                                     type="number"
                                     value={day}
                                     onChange={(e) => setDay(e.target.value)}
-                                    className="border-2 border-gray-300 p-2 rounded-md  dark:text-black "
+                                    className={`border-2 border-gray-300 p-2 rounded-md ${darkMode ? 'bg-gray-700 text-white' : 'bg-white text-black'}`}
                                     placeholder="Day"
                                     required
                                     min="1"
@@ -113,7 +108,7 @@ const SplashPage: React.FC<SplashPageProps> = ({ onAccessGranted }) => {
                                     type="number"
                                     value={month}
                                     onChange={(e) => setMonth(e.target.value)}
-                                    className="border-2 border-gray-300 p-2 rounded-md dark:text-black :"
+                                    className={`border-2 border-gray-300 p-2 rounded-md ${darkMode ? 'bg-gray-700 text-white' : 'bg-white text-black'}`}
                                     placeholder="Month"
                                     required
                                     min="1"
@@ -123,7 +118,7 @@ const SplashPage: React.FC<SplashPageProps> = ({ onAccessGranted }) => {
                                     type="number"
                                     value={year}
                                     onChange={(e) => setYear(e.target.value)}
-                                    className="border-2 border-gray-300 p-2 rounded-md dark:text-black "
+                                    className={`border-2 border-gray-300 p-2 rounded-md ${darkMode ? 'bg-gray-700 text-white' : 'bg-white text-black'}`}
                                     placeholder="Year"
                                     required
                                     min="1900"
@@ -150,6 +145,7 @@ const SplashPage: React.FC<SplashPageProps> = ({ onAccessGranted }) => {
             </div>
         </div>
     );
-}
+};
 
 export default SplashPage;
+
